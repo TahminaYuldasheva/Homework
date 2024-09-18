e61c44554252d86b4416e0587012496a2339ede9
Tahmina Yuldasheva <Tahmina Yuldasheva@DESKTOP-3M95QQV>
2024-09-18 21:10:49 +0200

    Add CreditCard class with deposit, withdraw, and info methods

---
+++ src/Task6/ClassWithMethods.java
@@ -0,0 +1,40 @@
+package Task6;
+
+public class ClassWithMethods {
+    public static class CreditCard {
+        String accountNumber;
+        double currentSum;
+
+        public CreditCard(double initialSum, String accountNumber) {
+            this.accountNumber = accountNumber;
+            if (initialSum >= 0) {
+                this.currentSum=initialSum;
+            } else {
+                System.out.println("Initial sum cannot be less than 0. ");
+                this.currentSum = 0;
+            }
+        }
+
+        public void addMoney(double amount) {
+            if (amount > 0) {
+                currentSum += amount;
+            } else {
+                System.out.println("Amount to add must be greater than 0.");
+            }
+        }
+        public void withdrawMoney(double amount) {
+            if(amount>0&&currentSum>=amount) {
+                currentSum-=amount;
+            }
+            else {
+                System.out.println("Not enough funds on the account.");
+            }
+            System.out.println();
+        }
+        public void showInfo () {
+            System.out.println("Account number " + accountNumber);
+            System.out.println("Current account balance " + currentSum);
+            System.out.println();
+        }
+    }
+}
